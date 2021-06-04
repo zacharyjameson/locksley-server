41ee7611e7cae05e80e519f34f0f8f9ea4f852bc
Zack Jameson <zacharymjameson@gmail.com>
2021-06-04 10:27:12 -0400

    stock fixtures

---
+++ test/locksley-fixtures.js
@@ -0,0 +1,49 @@
+function makeStockArray() {
+  return [
+    {
+      id: 1,
+      date_info: "2021-06-04",
+      price_open: "124.04000",
+      price_high: "125.36000",
+      price_low: "123.85000",
+      price_close: "124.92000",
+      volume: "18013682",
+    },
+    {
+      id: 2,
+      date_info: "2021-06-03",
+      price_open: "124.68000",
+      price_high: "124.85000",
+      price_low: "123.13000",
+      price_close: "123.54000",
+      volume: "76101500",
+    },
+    {
+      id: 3,
+      date_info: "2021-06-02",
+      price_open: "124.28800",
+      price_high: "125.24000",
+      price_low: "124.05000",
+      price_close: "125.09000",
+      volume: "52240362",
+    },
+    {
+      id: 4,
+      date_info: "2021-06-01",
+      price_open: "125.08000",
+      price_high: "125.35000",
+      price_low: "123.94500",
+      price_close: "124.28000",
+      volume: "67637118",
+    },
+    {
+      id: 5,
+      date_info: "2021-05-28",
+      price_open: "125.57000",
+      price_high: "125.80000",
+      price_low: "124.55000",
+      price_close: "124.61000",
+      volume: "71232700",
+    },
+  ];
+}
