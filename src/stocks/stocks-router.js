b108ecb04866194b420eb6b25922555148668525
Zack Jameson <zacharymjameson@gmail.com>
2021-06-05 10:15:45 -0400

    return all info for each stock

--- src/stocks/stocks-router.js
+++ src/stocks/stocks-router.js
@@ -18,69 +18,86 @@ const serializeStock = (stock) => ({
 });
 
 stockRouter
-    .route("/")
-    .get((req, res, next) => {
-        const knexInstance = req.app.get("db");
-        StockService.getAllStocks(knexInstance)
-            .then((stocks) => {
-                res.json(stocks.map(serializeStock));
-            })
-            .catch(next);
-    })
-    .post(jsonParser, (req, res, next) => {
-        const { stock_symbol, stock_volume, date_info, price_close, price_high, price_low, price_open } = req.body;
-        const newStock = { stock_symbol, stock_volume, date_info, price_close, price_high, price_low, price_open};
+  .route("/")
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    StockService.getAllStocks(knexInstance)
+      .then((stocks) => {
+        res.json(stocks.map(serializeStock));
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const {
+      stock_symbol,
+      stock_volume,
+      date_info,
+      price_close,
+      price_high,
+      price_low,
+      price_open,
+    } = req.body;
+    const newStock = {
+      stock_symbol,
+      stock_volume,
+      date_info,
+      price_close,
+      price_high,
+      price_low,
+      price_open,
+    };
 
-        for(const [key, value] of Object.entries(newStock)) {
-            if(!value) {
-                return res.status(400).json({
-                    error: { message: `Missing ${key} in request body`},
-                });
-            }
-        }
-
-        StockService.insertStock(req.app.get("db"), newStock)
-            .then((stock) => {
-                res
-                    .status(201)
-                    .location(path.posix.join(req.originalUrl, `/${stock.stock_symbol}`))
-                    .json(serializeStock(stock));
-            })
-            .catch(next);
-    })
-    .delete((req, res, next) => {
-        const knexInstance = req.app.get("db");
-        StockService.clearAllStocks(knexInstance).then((numRowsAffected) => {
-            res.status(204).end();
+    for (const [key, value] of Object.entries(newStock)) {
+      if (!value) {
+        return res.status(400).json({
+          error: { message: `Missing ${key} in request body` },
         });
+      }
+    }
+
+    StockService.insertStock(req.app.get("db"), newStock)
+      .then((stock) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${stock.stock_symbol}`))
+          .json(serializeStock(stock));
+      })
+      .catch(next);
+  })
+  .delete((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    StockService.clearAllStocks(knexInstance).then((numRowsAffected) => {
+      res.status(204).end();
     });
+  });
 
-    stockRouter
-        .route("/:stock_symbol")
-        .all((req, res, next) => {
-            const knexInstance = req.app.get("db");
-            StockService.getBySymbol(knexInstance, req.params.stock_symbol)
-                .then((stock) => {
-                    if(!stock){
-                        return res.status(404).json({
-                            error: { message: `Stock doesn't exist` },
-                        });
-                    }
-                    res.stock = stock;
-                    next();
-                })
-                .catch(next);
-        })
-        .get((req, res, next) => {
-            res.json(serializeStock(res.stock));
-        })
-        .delete((req, res, next) => {
-            const knexInstance = req.app.get("db");
-            StockService.deleteStock(knexInstance, req.params.stock_symbol)
-                .then((numRowsAffected) => {
-                    res.status(204).end();
-                })
-                .catch(next);
-        })
+stockRouter
+  .route("/:stock_symbol")
+  .all((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    StockService.getBySymbol(knexInstance, req.params.stock_symbol)
+      .then((stocks) => {
+        if (!stocks) {
+          return res.status(404).json({
+            error: { message: `Stock doesn't exist` },
+          });
+        } else {
+          res.json(stocks.map(serializeStock));
+        }
+      })
 
-        module.exports = stockRouter;
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeStock(res.stock));
+  })
+  .delete((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    StockService.deleteStock(knexInstance, req.params.stock_symbol)
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = stockRouter;
