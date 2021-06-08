c84bfb9762c1286c6d9922bbbaaa88eabee648fb
Zack Jameson <zacharymjameson@gmail.com>
2021-06-08 09:54:30 -0400

    seed change

--- seeds/seed.locksley_stocks.sql
+++ seeds/seed.locksley_stocks.sql
@@ -2,11 +2,9 @@ INSERT INTO locksley_stocks (
         stock_symbol,
         date_info,
         price_close,
-        stock_volume
     )
 VALUES (
         'AAPL',
         '2021-06-04',
         '124.92000',
-        '18013682'
     )
