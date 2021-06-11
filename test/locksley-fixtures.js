4b7b0815abb1e3b187b6f269dc619dacd37f28fc
Zack Jameson <zacharymjameson@gmail.com>
2021-06-11 16:53:56 -0400

    format

--- test/locksley-fixtures.js
+++ test/locksley-fixtures.js
@@ -1,17 +1,17 @@
 function makeStockArray() {
   return [
     {
-      fiftytwo_week_high: '263.19000',
-    fiftytwo_week_low: '184.00999',
-    id: 1,
-    stock_close: '257.40991',
-    stock_name: 'Microsoft Corp',
-    stock_open: '249.98000',
-    stock_percent_change: '2.63963',
-    stock_previous_close: '250.78999',
-    stock_symbol: 'MSFT',
-    stock_volume: '10284461'
-    }
+      fiftytwo_week_high: "263.19000",
+      fiftytwo_week_low: "184.00999",
+      id: 1,
+      stock_close: "257.40991",
+      stock_name: "Microsoft Corp",
+      stock_open: "249.98000",
+      stock_percent_change: "2.63963",
+      stock_previous_close: "250.78999",
+      stock_symbol: "MSFT",
+      stock_volume: "10284461",
+    },
   ];
 }
 
